542f5c9125459b59e5ba10399adec724fd426af6
d-kang <david.kang714@gmail.com>
2017-06-13 23:43:59 -0700

    Moved renderTweets To App Component

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,16 +7,10 @@ class Button extends React.Component {
     event.preventDefault();
     console.log(streams.home[0].message)
   }
-  renderTweets() {
-    return streams.home.map(function(home) {
-      return <div>@ {home.user}: {home.message} {String(home['created_at']).slice(17,24)}</div>
-    })
-  }
   render() {
     return (
       <div>
         <button onClick={(e) => this.helloButton(e)}>Render Tweets</button>;
-        <div className="allTweets">{this.renderTweets()}</div>
       </div>
     )
 
@@ -24,6 +18,3 @@ class Button extends React.Component {
 }
 
 export default Button
-
-
-// streams.home[0].message
