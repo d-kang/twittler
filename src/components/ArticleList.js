75fea512dcf9dcdcc7b18b6abeb881a84ebdd381
d-kang <david.kang714@gmail.com>
2017-06-23 20:10:05 -0700

    Refactored Clock.js

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import AddTweetForm from './AddTweetForm'
 import { streams } from '../data_generator';
-class ArticleList extends React.Component {
+export default class ArticleList extends React.Component {
   constructor(props) {
     super(props)
     this.state = { streams }
     this.renderTweets = this.renderTweets.bind(this)
+    this.userTweet = this.userTweet.bind(this)
   }
 
-  componentDidMount() {
-    this.timerID = setInterval( () => this.updateStreams(), 1000 );
-  }
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
   updateStreams() {
     this.setState( {streams} );
   }
   userTweet(e) {
     e.preventDefault();
-    console.log('hi')
+    console.log('this (userTweet)', this)
+    // this.setState({userName: this.state.streams.users[this.userSpan.id]})
+    console.log(this.state.streams.users[this.userSpan.id]);
+    console.log(this.userSpan.id)
   }
   renderTweets(messages) {
+    var self = this;
     return messages.home.map(function(home, i) {
       return (
         <div key={i}>
-          @ <span onClick={e=> this.userTweet(e)} className={'username ' + home.user}>{home.user}</span>: {home.message} {home['created_at'].toLocaleTimeString()}
+          @ <span onClick={self.userTweet} ref={input=>self.userSpan = input} className={'username ' + home.user} id={home.user}>{home.user}</span>: {home.message} {home['created_at'].toLocaleTimeString()}
         </div>
       )
     })
@@ -41,4 +40,134 @@ class ArticleList extends React.Component {
   }
 }
 
-export default ArticleList;
+
+// streams.home is an array of tweet objects
+//
+//
+// streams.users is an object with each user as a property
+// streams.users.mracus is an array of tweet objects
+
+// this.state.streams =  {
+//   "home": [
+//     {
+//       "user": "mracus",
+//       "message": " installed your system #sf",
+//       "created_at": "2017-06-23T17:26:37.538Z"
+//     },
+//     {
+//       "user": "douglascalhoun",
+//       "message": "just formulated my cat but only i know how",
+//       "created_at": "2017-06-23T17:26:37.538Z"
+//     },
+//     {
+//       "user": "sharksforcheap",
+//       "message": "ask me how i navigated my life #techlife",
+//       "created_at": "2017-06-23T17:26:37.538Z"
+//     },
+//     {
+//       "user": "mracus",
+//       "message": "efficiently overhauled this belief system #sxsw",
+//       "created_at": "2017-06-23T17:26:37.538Z"
+//     },
+//     {
+//       "user": "shawndrost",
+//       "message": "last night i systematized a belief system #magic",
+//       "created_at": "2017-06-23T17:26:37.538Z"
+//     },
+//     {
+//       "user": "douglascalhoun",
+//       "message": " built my koolaid ",
+//       "created_at": "2017-06-23T17:26:37.538Z"
+//     },
+//     {
+//       "user": "mracus",
+//       "message": "a ninja enjoyed your worm #magic",
+//       "created_at": "2017-06-23T17:26:37.538Z"
+//     },
+//     {
+//       "user": "douglascalhoun",
+//       "message": " formulated a future but only i know how",
+//       "created_at": "2017-06-23T17:26:37.538Z"
+//     },
+//     {
+//       "user": "sharksforcheap",
+//       "message": "just overhauled the big life #magic",
+//       "created_at": "2017-06-23T17:26:37.538Z"
+//     },
+//     {
+//       "user": "douglascalhoun",
+//       "message": "completely debugged an entire pony #techlife",
+//       "created_at": "2017-06-23T17:26:37.538Z"
+//     },
+//     {
+//       "user": "douglascalhoun",
+//       "message": " invented this worm ",
+//       "created_at": "2017-06-23T17:26:37.538Z"
+//     }
+//   ],
+//   "users": {
+//     "shawndrost": [
+//       {
+//         "user": "shawndrost",
+//         "message": "last night i systematized a belief system #magic",
+//         "created_at": "2017-06-23T17:26:37.538Z"
+//       }
+//     ],
+//     "sharksforcheap": [
+//       {
+//         "user": "sharksforcheap",
+//         "message": "ask me how i navigated my life #techlife",
+//         "created_at": "2017-06-23T17:26:37.538Z"
+//       },
+//       {
+//         "user": "sharksforcheap",
+//         "message": "just overhauled the big life #magic",
+//         "created_at": "2017-06-23T17:26:37.538Z"
+//       }
+//     ],
+//     "mracus": [
+//       {
+//         "user": "mracus",
+//         "message": " installed your system #sf",
+//         "created_at": "2017-06-23T17:26:37.538Z"
+//       },
+//       {
+//         "user": "mracus",
+//         "message": "efficiently overhauled this belief system #sxsw",
+//         "created_at": "2017-06-23T17:26:37.538Z"
+//       },
+//       {
+//         "user": "mracus",
+//         "message": "a ninja enjoyed your worm #magic",
+//         "created_at": "2017-06-23T17:26:37.538Z"
+//       }
+//     ],
+//     "douglascalhoun": [
+//       {
+//         "user": "douglascalhoun",
+//         "message": "just formulated my cat but only i know how",
+//         "created_at": "2017-06-23T17:26:37.538Z"
+//       },
+//       {
+//         "user": "douglascalhoun",
+//         "message": " built my koolaid ",
+//         "created_at": "2017-06-23T17:26:37.538Z"
+//       },
+//       {
+//         "user": "douglascalhoun",
+//         "message": " formulated a future but only i know how",
+//         "created_at": "2017-06-23T17:26:37.538Z"
+//       },
+//       {
+//         "user": "douglascalhoun",
+//         "message": "completely debugged an entire pony #techlife",
+//         "created_at": "2017-06-23T17:26:37.538Z"
+//       },
+//       {
+//         "user": "douglascalhoun",
+//         "message": " invented this worm ",
+//         "created_at": "2017-06-23T17:26:37.538Z"
+//       }
+//     ]
+//   }
+// }
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,31 +1,16 @@
 import React from 'react';
 
-// export function Clock(props) {
-//   return (
-//     <div>
-//       <h2 className='clock'>It is {props.date.toLocaleTimeString()}.</h2>
-//     </div>
-//   );
-// }
-
-// function tick() {
-//   ReactDOM.render(
-//     <Clock date ={new Date()} />,
-//     document.getElementById('main')
-//   );
-// }
-
-// setInterval(tick, 1000);
-function FormattedDate(props) {
-  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
-}
-
-
-class Clock extends React.Component {
+export default class Clock extends React.Component {
   constructor(props) {
     super(props)
     this.state = {date: new Date()};
+    this.formatedDate = this.formatedDate.bind(this)
   }
+
+  formatedDate() {
+    return <h2>It is {this.state.date.toLocaleTimeString()}.</h2>;
+  }
+
   componentDidMount() {
     this.timerID = setInterval( () => this.tick(), 1000 );
   }
@@ -43,13 +28,8 @@ class Clock extends React.Component {
   render() {
     return (
       <div>
-         <h1>Hello, world!</h1>
-         <FormattedDate date={this.state.date} />
-         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <h4>{this.formatedDate()}</h4>
        </div>
     );
   }
 }
-
-
-export default Clock;
